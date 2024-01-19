3aff54bdaf7830a8dca0b77b427b4463bd4c8326
kirkol94 <kirkol94@ya.ru>
2024-01-19 21:20:06 +0500

    почистил app.tsx

--- client/src/app/ui/App.tsx
+++ client/src/app/ui/App.tsx
@@ -2,21 +2,15 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "../styles/index.scss";
 import { Title, TitleSize } from "@/shared/ui/Title";
 import { Text, TextSize } from "@/shared/ui/Text";
+import { AppLink, AppLinkSize } from "@/shared/ui/AppLink";
+import { Button, ButtonSize } from "@/shared/ui/Button";
 
 export const App = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route index element={<>home</>} />
-        <Route
-          path="login"
-          element={
-            <>
-              <Title size={TitleSize.XXL}>Авторизация</Title>
-              <Text size={TextSize.S}>У вас уже есть учётная запись</Text>
-            </>
-          }
-        />
+        <Route path="login" element={<>login</>} />
         <Route path="register" element={<>register</>} />
         <Route path="*" element={<>page is not found</>} />
       </Routes>
