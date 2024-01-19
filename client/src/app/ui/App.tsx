76927a3993f224868acb3ddebb90da3b9adb6f0e
kirkol94 <kirkol94@ya.ru>
2024-01-19 20:22:56 +0500

    ui компонент AppLink

--- client/src/app/ui/App.tsx
+++ client/src/app/ui/App.tsx
@@ -1,12 +1,22 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "../styles/index.scss";
+import { Title, TitleSize } from "@/shared/ui/Title";
+import { Text, TextSize } from "@/shared/ui/Text";
 
 export const App = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route index element={<>home</>} />
-        <Route path="login" element={<>login</>} />
+        <Route
+          path="login"
+          element={
+            <>
+              <Title size={TitleSize.XXL}>Авторизация</Title>
+              <Text size={TextSize.S}>У вас уже есть учётная запись</Text>
+            </>
+          }
+        />
         <Route path="register" element={<>register</>} />
         <Route path="*" element={<>page is not found</>} />
       </Routes>
