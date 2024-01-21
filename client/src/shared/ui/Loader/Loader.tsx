e9ad561a32aa3e21321a7512d9a7021d33c47b00
Maxim Volkov <ketkette7@gmail.com>
2024-01-21 23:58:08 +0400

    Сделал loader

---
+++ client/src/shared/ui/Loader/Loader.tsx
@@ -0,0 +1,7 @@
+import classes from "./Loader.module.scss";
+
+
+export const Loader = () => {
+    return <span className={classes.loader}><div></div><div></div><div></div><div></div></span>     
+}
+ 
