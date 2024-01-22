260baa35e87955c17ff83a201a5c2aae3c70509e
kirkol94 <kirkol94@ya.ru>
2024-01-23 01:27:18 +0500

    добавил отображение имени после регистрации при редиректе

--- client/src/pages/HomePage/HomePage.tsx
+++ client/src/pages/HomePage/HomePage.tsx
@@ -1,11 +1,15 @@
+import { useAppSelector } from "@/shared/hooks";
+import { Title, TitleSize } from "@/shared/ui/Title";
 import { Footer } from "@/widgets/Footer";
 import cx from "classix";
 
 export const HomePage = () => {
   const className = cx("home__container");
+  const { name } = useAppSelector((state) => state.auth.user);
 
   return (
     <div className={className}>
+      <Title size={TitleSize.XXL}>Привет, {name}</Title>
       <Footer />
     </div>
   );
