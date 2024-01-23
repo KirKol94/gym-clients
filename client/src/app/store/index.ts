fc2eb83d5ce520f60912f417a631d1b76ecb6b30
kirkol94 <kirkol94@ya.ru>
2024-01-23 13:31:45 +0500

    файл store.ts > store/config/store.ts

--- client/src/app/store/index.ts
+++ client/src/app/store/index.ts
@@ -1,2 +1,2 @@
-export { store } from "./store";
+export { store } from "./config/store";
 export type { RootState, AppDispatch } from './types/types'
--- client/src/app/store/types/types.ts
+++ client/src/app/store/types/types.ts
@@ -1,4 +1,4 @@
-import { store } from '../store'
+import { store } from '../config/store'
 
 export type RootState = ReturnType<typeof store.getState>
 export type AppDispatch = typeof store.dispatch
