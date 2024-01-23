dd7107673bb93332a8ee2f8ae9b3b2e3117ae854
kirkol94 <kirkol94@ya.ru>
2024-01-23 21:30:07 +0500

    если пользователь не авторизован сделал редирект на страницу логина

--- client/src/pages/HomePage/ui/HomePage.tsx
+++ client/src/pages/HomePage/ui/HomePage.tsx
@@ -1,9 +1,24 @@
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import cx from "classix";
 
+import { getIsAuth } from "@/entities/User";
+import { ROUTER_PATH } from "@/shared/const/path/PATH";
+import { useAppSelector } from "@/shared/hooks";
 import { Footer } from "@/widgets/Footer";
 
 export const HomePage = () => {
+  const navigate = useNavigate();
+
+  const isAuth = useAppSelector(getIsAuth);
+
   const className = cx("home__container");
+
+  useEffect(() => {
+    if (!isAuth) {
+      navigate(ROUTER_PATH.LOGIN);
+    }
+  }, []);
 
   return (
     <div className={className}>
