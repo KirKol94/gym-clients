d9aa89826738cd8be4391d11c6ebbb3ff2735fb0
Maxim Volkov <ketkette7@gmail.com>
2024-01-24 08:54:13 +0400

    Пофиксил линтер

--- client/src/pages/NotFoundPage/ui/NotFoundPage.tsx
+++ client/src/pages/NotFoundPage/ui/NotFoundPage.tsx
@@ -1,9 +1,12 @@
 import cx from "classix";
-import classes from "./NotFoundPage.module.scss";
-import NotFoundImg from "../assets/image/notfound.svg?react";
-import { Title, TitleSize } from "@/shared/ui/Title";
-import { AppLink, AppLinkSize } from "@/shared/ui/AppLink";
+
 import { ROUTER_PATH } from "@/shared/const/path/PATH";
+import { AppLink, AppLinkSize } from "@/shared/ui/AppLink";
+import { Title, TitleSize } from "@/shared/ui/Title";
+
+import NotFoundImg from "../assets/image/notfound.svg?react";
+
+import classes from "./NotFoundPage.module.scss";
 
 export const NotFoundPage = () => {
   const className = cx("notfound__container", classes.page);
