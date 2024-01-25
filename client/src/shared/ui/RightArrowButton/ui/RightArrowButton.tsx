9c66acd6a7b56d59d1007539f3fe712ac3c7d8bf
Maxim Volkov <ketkette7@gmail.com>
2024-01-25 23:40:52 +0400

    добавил направления для стрелки

--- client/src/shared/ui/RightArrowButton/ui/RightArrowButton.tsx
+++ client/src/shared/ui/RightArrowButton/ui/RightArrowButton.tsx
@@ -1,12 +1,26 @@
-import rightArrow from "../assets/image/arrow.svg";
+import cx from "classix";
+
+import RightArrow from "../assets/image/arrow.svg?react";
+import { RightArrowButtonDirection } from "../model/types/rightArrowButton";
 
 import classes from "./RightArrowButton.module.scss";
 
+interface RightArrowButtonInfo {
+    direction?: RightArrowButtonDirection;
+}
 
-export const RightArrowButton = () => {
+export const RightArrowButton = ({ direction }: RightArrowButtonInfo) => {
+
+    const cls = cx(
+        classes.arrow,
+        direction === RightArrowButtonDirection.U && classes.arrow__u,
+        direction === RightArrowButtonDirection.B && classes.arrow__b,
+        direction === RightArrowButtonDirection.L && classes.arrow__l,
+    )
+
     return ( 
-        <button className={classes["right-arrow-button"]}>
-            <img src={rightArrow} />
+        <button className={cls}>
+            <RightArrow />
         </button>
      );
 }
