0ec701ee4cec107d337f7e56edb1b475896050b9
PavelHiga <katashihiga@mail.ru>
2024-01-27 18:20:56 +0700

    Заменил обычный текст на Title

--- client/src/widgets/Sidebar/SidebarItem/ui/SidebarItem.tsx
+++ client/src/widgets/Sidebar/SidebarItem/ui/SidebarItem.tsx
@@ -1,6 +1,8 @@
 import { useNavigate } from 'react-router-dom'
 import cx from 'classix'
 
+import { Text, TextSize } from '@/shared/ui/Text'
+
 import classes from './SidebarItem.module.scss'
 
 interface SidebarItemProps {
@@ -16,6 +18,7 @@ export const SidebarItem = ({ data, active, setActive, open }: SidebarItemProps)
 
   const cardOpen = cx(active === title ? classes.card__active : classes.card)
   const cardClose = cx(active === title ? classes.card__close__active : classes.card__close)
+  const textClass = cx(active === title ? classes.text__active : classes.text)
 
   const cardClass = cx(open ? cardOpen : cardClose)
   const bgClass = cx(active === title && classes.background)
@@ -29,7 +32,11 @@ export const SidebarItem = ({ data, active, setActive, open }: SidebarItemProps)
     <div className={classes.wrapper}>
       <li className={cardClass} onClick={handeleClick}>
         <img src={link} alt={title} />
-        {open && <p>{title}</p>}
+        {open && (
+          <Text className={textClass} size={TextSize.S}>
+            {title}
+          </Text>
+        )}
       </li>
       <div className={bgClass} />
     </div>
