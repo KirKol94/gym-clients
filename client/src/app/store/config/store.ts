b2aa5d71e538495692597759785de358d3f51740
kirkol94 <kirkol94@ya.ru>
2024-01-27 19:05:38 +0500

    инстанс rtkApi

--- client/src/app/store/config/store.ts
+++ client/src/app/store/config/store.ts
@@ -2,12 +2,15 @@ import { combineReducers, configureStore } from '@reduxjs/toolkit'
 
 import { userReducer } from '@/entities/User'
 import { authReducer } from '@/features/AuthForm'
+import { rtkApi } from '@/shared/api/rtkApi'
 
 const rootReducer = combineReducers({
   user: userReducer,
   auth: authReducer,
+  [rtkApi.reducerPath]: rtkApi.reducer,
 })
 
 export const store = configureStore({
   reducer: rootReducer,
+  middleware: (getDefaultMiddleWare) => getDefaultMiddleWare().concat(rtkApi.middleware),
 })
