99b8ec32b1fce569a4cf5c96d957c9e4f17facae
kirkol94 <kirkol94@ya.ru>
2024-01-28 21:25:26 +0500

    в компонент HomePage для теста на кнопку со стрелочкой повесил функцию handleLogout

--- client/src/pages/HomePage/ui/HomePage.tsx
+++ client/src/pages/HomePage/ui/HomePage.tsx
@@ -2,19 +2,32 @@ import { useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
 import cx from 'classix'
 
-import { getIsAuth } from '@/entities/User'
+import { getIsAuth, userActions } from '@/entities/User'
+import { ACCESS_TOKEN_LOCAL_STORAGE_KEY } from '@/shared/const/localStorage/accessTokenKey'
+import { IS_AUTH_LOCAL_STORAGE_KEY } from '@/shared/const/localStorage/isAuthKey'
+import { USER_LOCAL_STORAGE_KEY } from '@/shared/const/localStorage/userKey'
 import { ROUTER_PATH } from '@/shared/const/path/PATH'
-import { useAppSelector } from '@/shared/hooks'
+import { useAppDispatch, useAppSelector } from '@/shared/hooks'
 import { ButtonWithArrow } from '@/shared/ui/ButtonWithArrow'
 import { AlertCard } from '@/widgets/AlertCard'
 import { Footer } from '@/widgets/Footer'
 
 export const HomePage = () => {
   const navigate = useNavigate()
+  const dispatch = useAppDispatch()
 
   const isAuth = useAppSelector(getIsAuth)
 
   const className = cx('home__container')
+
+  // TODO это должно быть в итоге в feature Logout, но пока такого компонента нет
+  const handleLogout = () => {
+    dispatch(userActions.clearUserData())
+
+    localStorage.removeItem(IS_AUTH_LOCAL_STORAGE_KEY)
+    localStorage.removeItem(ACCESS_TOKEN_LOCAL_STORAGE_KEY)
+    localStorage.removeItem(USER_LOCAL_STORAGE_KEY)
+  }
 
   useEffect(() => {
     if (!isAuth) {
@@ -29,7 +42,7 @@ export const HomePage = () => {
         title="Контрагента ожидают в стадии “Подписание контракта”"
         alert="Подпишите контракты с контрагентами или переведите их в архивные"
       />
-      <ButtonWithArrow />
+      <ButtonWithArrow onClick={handleLogout} />
       <Footer />
     </div>
   )
