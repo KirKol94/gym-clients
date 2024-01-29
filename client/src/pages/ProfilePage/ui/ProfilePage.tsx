b2588d62393d2fc70af43be53d65ae59bdfc259c
kirkol94 <kirkol94@ya.ru>
2024-01-29 11:43:07 +0500

    избавился от проверки в ProfilePage и обернул его в компонент Page (с лайаутом внутри)

--- client/src/pages/ProfilePage/ui/ProfilePage.tsx
+++ client/src/pages/ProfilePage/ui/ProfilePage.tsx
@@ -1,23 +1,7 @@
-import { useEffect } from 'react'
-import { useNavigate } from 'react-router-dom'
-
-import { getIsAuth } from '@/entities/User'
-import { ROUTER_PATH } from '@/shared/const/path/PATH'
-import { useAppSelector } from '@/shared/hooks'
 import { AlertCard } from '@/widgets/AlertCard'
 import Page from '@/widgets/Page'
 
 export const ProfilePage = () => {
-  const navigate = useNavigate()
-
-  const isAuth = useAppSelector(getIsAuth)
-
-  useEffect(() => {
-    if (!isAuth) {
-      navigate(ROUTER_PATH.LOGIN)
-    }
-  }, [isAuth, navigate])
-
   return (
     <Page>
       <AlertCard
