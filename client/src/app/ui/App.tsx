e0b94dc7a7fa0c80845501ac76ed18283e9224ea
kirkol94 <kirkol94@ya.ru>
2024-02-01 23:38:21 +0500

    Убрал проверку авторизованности из App.tsx
    пофиксил тем самым ререндер при навигации по сайдбару

--- client/src/app/ui/App.tsx
+++ client/src/app/ui/App.tsx
@@ -1,28 +1,17 @@
 import { useEffect } from 'react'
-import { useNavigate } from 'react-router-dom'
 
 import { Router } from '@/app/routes'
-import { getIsAuth, userActions } from '@/entities/User'
-import { ROUTER_PATH } from '@/shared/const/path/PATH'
-import { useAppDispatch, useAppSelector } from '@/shared/hooks'
+import { userActions } from '@/entities/User'
+import { useAppDispatch } from '@/shared/hooks'
 
 import '../styles/index.scss'
 
 export const App = () => {
   const dispatch = useAppDispatch()
-  const navigate = useNavigate()
-  const isAuth = useAppSelector(getIsAuth)
 
   useEffect(() => {
     dispatch(userActions.initAuthData())
   }, [dispatch])
-
-  useEffect(() => {
-    if (isAuth) {
-      navigate(ROUTER_PATH.PROFILE)
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [isAuth])
 
   return <Router />
 }
