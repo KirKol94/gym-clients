33c2461ccb4691e57b65fd2398890ff0814b368c
kirkol94 <kirkol94@ya.ru>
2024-02-01 23:37:03 +0500

    Убрал ненужный лог из UsersPage
    при попадании на страницу users вываливался его путь в консоль

--- client/src/pages/UsersPage/ui/UsersPage.tsx
+++ client/src/pages/UsersPage/ui/UsersPage.tsx
@@ -1,11 +1,7 @@
-import { useLocation } from 'react-router-dom'
-
 import { Title, TitleSize } from '@/shared/ui/Title'
 import Page from '@/widgets/Page'
 
 export const UsersPage = () => {
-  const { pathname } = useLocation()
-  console.log(pathname)
   return (
     <Page>
       <Title size={TitleSize.XXL}>Users page</Title>
