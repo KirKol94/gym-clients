6a4506d4845f6c333f48d6758e881415a4d81fc7
kirkol94 <kirkol94@ya.ru>
2024-02-03 21:27:34 +0500

    поправил название класса в шаблоне компонента

--- client/scripts/createSlice/templates/componentTemplate.js
+++ client/scripts/createSlice/templates/componentTemplate.js
@@ -3,13 +3,13 @@ import firstCharUpperCase from '../firstCharUpperCase.js'
 
 export default (componentName) => `import cx from 'classix'
 
-import clx from './${firstCharUpperCase(componentName)}.module.scss'
+import cls from './${firstCharUpperCase(componentName)}.module.scss'
 
 interface ${firstCharUpperCase(componentName)}Props {}
 
 export const ${firstCharUpperCase(componentName)} = ({}: ${firstCharUpperCase(componentName)}Props) => {
-  const ${firstCharLowerCase(componentName)}Class = cx(clx.button)
+  const ${firstCharLowerCase(componentName)}Class = cx(cls.${firstCharLowerCase(componentName)}
 
-  return <div className={${firstCharLowerCase(componentName)}Class}>${componentName}</div>
+  return <div className={${firstCharLowerCase(componentName)}Class>${componentName}</div>
 }
 `
