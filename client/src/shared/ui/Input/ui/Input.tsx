39f6742a117c0a46a29cf3fd19707db1ffd48a9f
kirkol94 <kirkol94@ya.ru>
2024-02-05 13:12:17 +0500

    style: Input
    Добавил необязательный проп для Input `error` и немного стилизовал его. В этот проп будет передаваться ошибка при валидации полей

--- client/src/shared/ui/Input/ui/Input.tsx
+++ client/src/shared/ui/Input/ui/Input.tsx
@@ -1,4 +1,4 @@
-import { InputHTMLAttributes } from 'react'
+import { forwardRef, InputHTMLAttributes } from 'react'
 import cx from 'classix'
 
 import classes from './Input.module.scss'
@@ -6,13 +6,15 @@ import classes from './Input.module.scss'
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   inputName: string
   className?: string
+  error?: string
 }
 
-export const Input = ({ inputName, className, ...props }: InputProps) => {
+export const Input = forwardRef<HTMLInputElement, InputProps>(({ inputName, className, error, ...props }, ref) => {
   return (
     <div className={cx(classes.wrapper, className)}>
       <span className={classes.span}>{inputName}</span>
-      <input {...props} className={classes.input} />
+      <input ref={ref} {...props} className={classes.input} />
+      <div className={classes.error}>{error}</div>
     </div>
   )
-}
+})
