3b7a5b0a2c6ea40f4e08614bdfd442764d728ac7
Devergence <Devergence@outlook.com>
2024-02-06 23:34:48 +0500

    FE: новый компонент для маскированного инпута (#73)
    ---------
    
    Co-authored-by: Devergence <dmitriy.prikhodko@akbarsdigital.ru>

--- client/src/features/AddNewClientForm/ui/AddNewClientForm.tsx
+++ client/src/features/AddNewClientForm/ui/AddNewClientForm.tsx
@@ -1,7 +1,8 @@
-import { SubmitHandler, useForm } from 'react-hook-form'
+import { FormProvider, type SubmitHandler, useForm } from 'react-hook-form'
 import cx from 'classix'
 
 import { IClient } from '@/entities/Client'
+import { BaseMaskInput } from '@/shared/ui/BaseMaskInput'
 import { Button, ButtonSize } from '@/shared/ui/Button'
 import { Input } from '@/shared/ui/Input'
 import { Text, TextSize } from '@/shared/ui/Text'
@@ -13,12 +14,17 @@ import cls from './AddNewClientForm.module.scss'
 type ClientDataType = Omit<IClient, 'id'>
 
 export const AddNewClientForm = () => {
+  const methods = useForm<ClientDataType>({
+    mode: 'onChange',
+  })
+
   const {
     register,
     handleSubmit,
     reset,
     formState: { errors, isValid, isDirty },
-  } = useForm<ClientDataType>({ mode: 'onChange' })
+  } = methods
+
   const [addClient, { data: resClient, status }] = useAddNewClient()
 
   const onSubmit: SubmitHandler<ClientDataType> = (data) => {
@@ -31,156 +37,149 @@ export const AddNewClientForm = () => {
   }
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)} className={cx(cls.form)}>
-      <Input
-        {...register('firstName', {
-          required: 'Обязательное поле',
-          minLength: {
-            value: 2,
-            message: 'Не менее 2 символов',
-          },
-          maxLength: {
-            value: 15,
-            message: 'Не более 15 символов',
-          },
-          pattern: {
-            value: /^[а-яА-ЯёЁ-]*$/,
-            message: 'Только русские буквы и «-»',
-          },
-        })}
-        error={errors?.firstName?.message}
-        placeholder="Иван"
-        className={cls.input}
-        inputName="Имя"
-      />
+    <FormProvider {...methods}>
+      <form onSubmit={handleSubmit(onSubmit)} className={cx(cls.form)}>
+        <Input
+          {...register('firstName', {
+            required: 'Обязательное поле',
+            minLength: {
+              value: 2,
+              message: 'Не менее 2 символов',
+            },
+            maxLength: {
+              value: 15,
+              message: 'Не более 15 символов',
+            },
+            pattern: {
+              value: /^[а-яА-ЯёЁ-]*$/,
+              message: 'Только русские буквы и «-»',
+            },
+          })}
+          error={errors?.firstName?.message}
+          placeholder="Иван"
+          className={cls.input}
+          inputName="Имя"
+        />
 
-      <Input
-        {...register('middleName', {
-          required: 'Обязательное поле',
-          minLength: {
-            value: 2,
-            message: 'Не менее 2 символов',
-          },
-          maxLength: {
-            value: 24,
-            message: 'Не более 24 символов',
-          },
-          pattern: {
-            value: /^[а-яА-ЯёЁ-]*$/,
-            message: 'Только русские буквы и «-»',
-          },
-        })}
-        error={errors?.middleName?.message}
-        placeholder="Иванов"
-        className={cls.input}
-        inputName="Фамилия"
-      />
+        <Input
+          {...register('middleName', {
+            required: 'Обязательное поле',
+            minLength: {
+              value: 2,
+              message: 'Не менее 2 символов',
+            },
+            maxLength: {
+              value: 24,
+              message: 'Не более 24 символов',
+            },
+            pattern: {
+              value: /^[а-яА-ЯёЁ-]*$/,
+              message: 'Только русские буквы и «-»',
+            },
+          })}
+          error={errors?.middleName?.message}
+          placeholder="Иванов"
+          className={cls.input}
+          inputName="Фамилия"
+        />
 
-      <Input
-        {...register('lastName', {
-          required: 'Обязательное поле',
-          minLength: {
-            value: 6,
-            message: 'Не менее 6 символов',
-          },
-          maxLength: {
-            value: 20,
-            message: 'Не более 20 символов',
-          },
-          pattern: {
-            value: /^[а-яА-ЯёЁ]*$/,
-            message: 'Только русские буквы',
-          },
-        })}
-        error={errors?.lastName?.message}
-        placeholder="Иванович"
-        className={cls.input}
-        inputName="Отчество"
-      />
+        <Input
+          {...register('lastName', {
+            required: 'Обязательное поле',
+            minLength: {
+              value: 6,
+              message: 'Не менее 6 символов',
+            },
+            maxLength: {
+              value: 20,
+              message: 'Не более 20 символов',
+            },
+            pattern: {
+              value: /^[а-яА-ЯёЁ]*$/,
+              message: 'Только русские буквы',
+            },
+          })}
+          error={errors?.lastName?.message}
+          placeholder="Иванович"
+          className={cls.input}
+          inputName="Отчество"
+        />
 
-      <fieldset className={cls.gender}>
-        <label className={cls.gender__row}>
-          <Text size={TextSize.S}>Мужской</Text>
-          <input type="radio" value="1" defaultChecked {...register('sex')} />
-        </label>
-        <label className={cls.gender__row}>
-          <Text size={TextSize.S}>Женский</Text>
-          <input type="radio" value="0" {...register('sex')} />
-        </label>
-      </fieldset>
+        <fieldset className={cls.gender}>
+          <label className={cls.gender__row}>
+            <Text size={TextSize.S}>Мужской</Text>
+            <input type="radio" value="1" defaultChecked {...register('sex')} />
+          </label>
+          <label className={cls.gender__row}>
+            <Text size={TextSize.S}>Женский</Text>
+            <input type="radio" value="0" {...register('sex')} />
+          </label>
+        </fieldset>
 
-      <Input
-        {...register('birthday', {
-          pattern: {
-            value: /\d{2}\.\d{2}\.\d{4}/,
-            message: 'Дата должна быть в формате ДД.ММ.ГГГГ',
-          },
-        })}
-        error={errors?.birthday?.message}
-        placeholder="дд.мм.гггг"
-        className={cls.input}
-        inputName="Дата рождения"
-      />
+        <Input
+          {...register('birthday', {
+            pattern: {
+              value: /\d{2}\.\d{2}\.\d{4}/,
+              message: 'Дата должна быть в формате ДД.ММ.ГГГГ',
+            },
+          })}
+          error={errors?.birthday?.message}
+          placeholder="дд.мм.гггг"
+          className={cls.input}
+          inputName="Дата рождения"
+        />
 
-      <Input
-        {...register('email', {
-          pattern: {
-            value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
-            message: 'Введите валидный email',
-          },
-        })}
-        error={errors?.email?.message}
-        placeholder="ivan.ivanov@mail.ru"
-        className={cls.input}
-        inputName="Email"
-      />
+        <Input
+          {...register('email', {
+            pattern: {
+              value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+              message: 'Введите валидный email',
+            },
+          })}
+          error={errors?.email?.message}
+          placeholder="ivan.ivanov@mail.ru"
+          className={cls.input}
+          inputName="Email"
+        />
 
-      <Input
-        {...register('mobilePhone', {
-          minLength: {
-            value: 11,
-            message: 'Не менее 11 символов',
-          },
-          maxLength: {
-            value: 16,
-            message: 'Не более 16 символов',
-          },
-        })}
-        defaultValue="+79"
-        error={errors?.mobilePhone?.message}
-        placeholder="+7(999)999-99-99"
-        className={cls.input}
-        inputName="Телефон"
-      />
+        <BaseMaskInput
+          label="Телефон"
+          name="mobilePhone"
+          type="tel"
+          format="+7 (###) ###-##-##"
+          placeholder="+7 (999) 999-99-99"
+          className={cls.input}
+        />
 
-      <Input
-        type="tel"
-        {...register('personalTrainingCount', {
-          maxLength: {
-            value: 3,
-            message: 'Не длиннее 3 символов',
-          },
-          min: {
-            value: 1,
-            message: 'Минимум 1',
-          },
-          max: {
-            value: 100,
-            message: 'Значение не должно превышать 100',
-          },
-          pattern: {
-            value: /^[0-9]*$/,
-            message: 'Только цифры от 0 до 9',
-          },
-        })}
-        error={errors?.personalTrainingCount?.message}
-        placeholder="0"
-        className={cls.input}
-        inputName="Количество персональных тренировок"
-      />
-      <Button type="submit" size={ButtonSize.M} disabled={!isDirty || !isValid}>
-        Добавить
-      </Button>
-    </form>
+        <Input
+          type="tel"
+          {...register('personalTrainingCount', {
+            maxLength: {
+              value: 3,
+              message: 'Не длиннее 3 символов',
+            },
+            min: {
+              value: 1,
+              message: 'Минимум 1',
+            },
+            max: {
+              value: 100,
+              message: 'Значение не должно превышать 100',
+            },
+            pattern: {
+              value: /^[0-9]*$/,
+              message: 'Только цифры от 0 до 9',
+            },
+          })}
+          error={errors?.personalTrainingCount?.message}
+          placeholder="0"
+          className={cls.input}
+          inputName="Количество персональных тренировок"
+        />
+        <Button type="submit" size={ButtonSize.M} disabled={!isDirty || !isValid}>
+          Добавить
+        </Button>
+      </form>
+    </FormProvider>
   )
 }
