43e66b78db6bea2c9dc32cb123767e8760918a6d
kirkol94 <kirkol94@ya.ru>
2024-02-07 11:52:43 +0500

    fix: стили SidebarItem
    добавил анимацию на font-size, чтоб при сжимании не выстраивалось в 2 строки
    - немного сократил код
    - и в проп передал не картинку, а свг элемент

--- client/src/widgets/Sidebar/Sidebar/ui/Sidebar.tsx
+++ client/src/widgets/Sidebar/Sidebar/ui/Sidebar.tsx
@@ -1,16 +1,16 @@
 import { useEffect, useState } from 'react'
 import cx from 'classix'
 
-import Agent from '@/shared/assets/icons/Agent.svg'
-import Avatar from '@/shared/assets/icons/Avatar.svg'
+import Agent from '@/shared/assets/icons/Agent.svg?react'
+import Avatar from '@/shared/assets/icons/Avatar.svg?react'
 import BackArrow from '@/shared/assets/icons/Back.svg?react'
-import Contract from '@/shared/assets/icons/Contract.svg'
-import Deals from '@/shared/assets/icons/Deals.svg'
-import Group from '@/shared/assets/icons/Group.svg'
+import Contract from '@/shared/assets/icons/Contract.svg?react'
+import Deals from '@/shared/assets/icons/Deals.svg?react'
+import Group from '@/shared/assets/icons/Group.svg?react'
 import Logo from '@/shared/assets/icons/Logo.svg?react'
 import LogoMini from '@/shared/assets/icons/LogoMini.svg?react'
-import Setting from '@/shared/assets/icons/SettingsWhite.svg'
-import Users from '@/shared/assets/icons/UsersWhite.svg'
+import Setting from '@/shared/assets/icons/SettingsWhite.svg?react'
+import Users from '@/shared/assets/icons/UsersWhite.svg?react'
 import { IS_SIDEBAR_VISIBLE } from '@/shared/const/localStorage/sideBarVisible'
 import { ROUTER_PATH } from '@/shared/const/path/PATH'
 
@@ -24,37 +24,37 @@ export const Sidebar = () => {
   const sidebarItems = [
     {
       title: 'Иван Иванов',
-      icon: Avatar,
+      Icon: <Avatar />,
       to: ROUTER_PATH.PROFILE,
     },
     {
       title: 'Пользователи',
-      icon: Users,
+      Icon: <Users />,
       to: ROUTER_PATH.USERS,
     },
     {
       title: 'Группы компаний',
-      icon: Group,
+      Icon: <Group />,
       to: ROUTER_PATH.GROUP,
     },
     {
       title: 'Контрагенты',
-      icon: Agent,
+      Icon: <Agent />,
       to: ROUTER_PATH.AGENT,
     },
     {
       title: 'Контракты',
-      icon: Contract,
+      Icon: <Contract />,
       to: ROUTER_PATH.CONTRACT,
     },
     {
       title: 'Сделки',
-      icon: Deals,
+      Icon: <Deals />,
       to: ROUTER_PATH.DEALS,
     },
     {
       title: 'Настройки',
-      icon: Setting,
+      Icon: <Setting />,
       to: ROUTER_PATH.SETTINGS,
     },
   ]
