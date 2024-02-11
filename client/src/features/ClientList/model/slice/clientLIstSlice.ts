af29a53a826d487f9f5eff9cc9e5c09fe9011408
kirkol94 <kirkol94@ya.ru>
2024-02-11 22:42:46 +0500

    add redux-slice for the ClientList component

---
+++ client/src/features/ClientList/model/slice/clientLIstSlice.ts
@@ -0,0 +1,21 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit'
+
+import { IClient } from '@/entities/Client'
+
+import { initialState } from './initialState'
+
+const clientsSlice = createSlice({
+  name: 'clients',
+  initialState,
+  reducers: {
+    setClients: (state, action: PayloadAction<IClient[]>) => {
+      state.clients = action.payload
+    },
+    addNewClient: (state, action: PayloadAction<IClient>) => {
+      state.clients.push(action.payload)
+    },
+  },
+})
+
+export const { actions: clientsActions } = clientsSlice
+export const { reducer: clientsReducer } = clientsSlice
