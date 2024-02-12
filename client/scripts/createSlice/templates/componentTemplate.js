af205f58879a97fba5e44dd2d114e73c438746f3
kirkol94 <kirkol94@ya.ru>
2024-02-12 18:56:04 +0500

    поправил шаблон ui компонента, добавив обёртку memo

--- client/scripts/createSlice/templates/componentTemplate.js
+++ client/scripts/createSlice/templates/componentTemplate.js
@@ -1,15 +1,16 @@
 import firstCharLowerCase from '../firstCharLowerCase.js'
 import firstCharUpperCase from '../firstCharUpperCase.js'
 
-export default (componentName) => `import cx from 'classix'
+export default (componentName) => `import { memo } from 'react'
+import cx from 'classix'
 
 import cls from './${firstCharUpperCase(componentName)}.module.scss'
 
 interface ${firstCharUpperCase(componentName)}Props {}
 
-export const ${firstCharUpperCase(componentName)} = ({}: ${firstCharUpperCase(componentName)}Props) => {
-  const ${firstCharLowerCase(componentName)}Class = cx(cls.${firstCharLowerCase(componentName)}
+export const ${firstCharUpperCase(componentName)} = memo(({}: ${firstCharUpperCase(componentName)}Props) => {
+  const ${firstCharLowerCase(componentName)}Class = cx(cls.${firstCharLowerCase(componentName)})
 
   return <div className={${firstCharLowerCase(componentName)}Class}>${componentName}</div>
-}
+})
 `
