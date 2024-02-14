b58e0180d7b77eb9eb66715eab67772f56cc01d8
kirkol94 <kirkol94@ya.ru>
2024-02-15 01:05:14 +0500

    stories for ClientList

--- client/src/features/ClientList/model/slice/initialState.ts
+++ client/src/features/ClientList/model/slice/initialState.ts
@@ -5,5 +5,28 @@ type InitialState = {
 }
 
 export const initialState: InitialState = {
-  clients: [],
+  clients: [
+    {
+      id: 1,
+      firstName: 'Иван',
+      lastName: 'Иванов',
+      middleName: 'Иванович',
+      sex: 1,
+      email: 'ivan@ivan.ru',
+      mobilePhone: '+7(999)999-99-99',
+      birthday: '2000-02-01',
+      personalTrainingCount: 10,
+    },
+    {
+      id: 2,
+      firstName: 'Иван',
+      lastName: 'Иванов',
+      middleName: 'Иванович',
+      sex: 1,
+      email: 'ivan@ivan.ru',
+      mobilePhone: '+7(999)999-99-99',
+      birthday: '2000-02-01',
+      personalTrainingCount: 10,
+    },
+  ],
 }
