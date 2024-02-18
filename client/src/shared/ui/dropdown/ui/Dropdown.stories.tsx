27a783e6ae69b2f5156a91fd377d561a66ad893f
Ismail <hummetzadeismail@gmail.com>
2024-02-18 11:56:32 +0300

    FE: fix storybook for dropdown

--- client/src/shared/ui/dropdown/ui/Dropdown.stories.tsx
+++ client/src/shared/ui/dropdown/ui/Dropdown.stories.tsx
@@ -6,7 +6,26 @@ import { Dropdown } from './Dropdown'
 export default {
   title: 'shared/ui/Dropdown',
   component: Dropdown,
-  args: {},
+  args: {
+    options: [
+      {
+        label: 'hello',
+        value: 1,
+      },
+      {
+        label: 'hello1',
+        value: 2,
+      },
+      {
+        label: 'hello2',
+        value: 3,
+      },
+      {
+        label: 'hello3',
+        value: 4,
+      },
+    ],
+  },
 } as Meta
 
 const Template: Story<DropdownProps> = (args) => <Dropdown {...args} />
