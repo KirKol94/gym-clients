0cee8d5f313382e9731fb353b0563ee20061540b
kirkol94 <kirkol94@ya.ru>
2024-03-13 21:29:30 +0500

    заглушки для authRouter

---
+++ server/src/routes/authRouter.ts
@@ -0,0 +1,25 @@
+import { type Request, type Response, Router } from 'express'
+
+type LoginInputData = {
+  login: string
+  password: string
+}
+
+export const authRouter = (): Router => {
+  const router = Router()
+
+  //   обработка /auth/register
+  router.post('/register', (req: Request, res: Response) => {
+    // проверяем входные данные и если всё ок - закидываем в БД
+    res.send(req.body) // а это для теста пока
+  })
+
+  //   обработка /auth/login
+  router.post('/login', (req: Request<{}, {}, LoginInputData>, res: Response) => {
+    const { login, password } = req.body
+    // проверяем входные данные и если всё ок - отправляем JWT
+    res.send({ login, password })
+  })
+
+  return router
+}
