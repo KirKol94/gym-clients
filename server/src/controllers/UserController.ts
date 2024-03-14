55e058f88e82071859e74ce3d486d8f4990cb825
MaksWork <sauageog@gmail.com>
2024-03-14 16:28:04 +0200

    попытка сделать findOrCreate

--- server/src/controllers/UserController.ts
+++ server/src/controllers/UserController.ts
@@ -1,4 +1,5 @@
 import colors from 'colors'
+import type { Model } from 'sequelize'
 
 import { User } from '../db'
 import type { IUser } from '../types/IUser'
@@ -13,7 +14,10 @@ export const UserController = {
   // TODO вынести этот тип отсюда и из authRouter в IUser.ts
   saveUser: async (user: Pick<IUser, 'email' | 'password' | 'firstName' | 'lastName' | 'middleName'>) => {
     try {
-      const newUser = await User.create(user)
+      const [newUser, created]: [Model, boolean] = await User.findOrCreate(user)
+      if (created) {
+        throw new Error('Пользователь уже зарегистрирован')
+      }
       console.log('Новый пользователь успешно добавлен:', newUser.toJSON())
     } catch (error) {
       console.error('Ошибка при создании пользователя:', error)
