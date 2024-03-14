e162fa337872d811dc886e1ef4748cf27a952a39
kirkol94 <kirkol94@ya.ru>
2024-03-15 01:50:03 +0500

    валидация входных данных логина

--- server/src/routes/usersRouter.ts
+++ server/src/routes/usersRouter.ts
@@ -12,14 +12,28 @@ export const authRouter = (): Router => {
     '/register',
     [
       check('email', 'email не может быть пустым').notEmpty(),
-      check('password', 'password должен быть от 6 до 16 символов').isLength({ min: 6, max: 16 }),
-      check('firstName', 'firstName не может быть пустым').notEmpty(),
-      check('lastName', 'lastName не может быть пустым').notEmpty(),
+      check('password', 'пароль должен быть от 6 до 16 символов и не должен содержать пробелов')
+        .isLength({ min: 6, max: 16 })
+        .not()
+        .contains(' '),
+      check('firstName', 'имя должно быть не менее 2 и не более 20 символов').isLength({ min: 2, max: 20 }),
+      check('lastName', 'фамилия должна быть не менее 2 и не более 24 символов').isLength({ min: 2, max: 24 }),
+      check('middleName', 'отчество должно быть не менее 6 и не более 24 символов').isLength({ min: 2, max: 24 }),
     ],
     UserController.registerUser,
   )
 
-  router.post('/login', UserController.loginUser)
+  router.post(
+    '/login',
+    [
+      check('email', 'Не корректный email').isEmail(),
+      check('password', 'пароль должен быть от 6 до 16 символов и не должен содержать пробелов')
+        .isLength({ min: 6, max: 16 })
+        .not()
+        .contains(' '),
+    ],
+    UserController.loginUser,
+  )
 
   return router
 }
