1955a78f44f67964a47045a41dde6e027bf03852
kirkol94 <kirkol94@ya.ru>
2024-03-14 10:59:48 +0500

    поправлены типы
    вынес глобальный тип Empty
    определил interface пользователя IUser
    определил типы входных данных для `auth/` роутера

--- server/src/global.d.ts
+++ server/src/global.d.ts
@@ -1,1 +1,2 @@
+// глобальный тип который будет доступен без импортов
 declare type Empty = Record<string, never>
--- server/src/routes/auth/types.ts
+++ server/src/routes/auth/types.ts
@@ -1,4 +1,7 @@
-export type LoginInputData = {
-  email: string
-  password: string
-}
+import { type IUser } from '../../types/IUser'
+
+// из типа IUser достаём следующие поля для определения в типе
+export type RegisterInputData = Pick<IUser, 'email' | 'password' | 'firstName' | 'lastName' | 'middleName'>
+
+// из типа RegisterInputData достаём поля email | password и определяем их в следующем типе
+export type LoginInputData = Pick<RegisterInputData, 'email' | 'password'>
