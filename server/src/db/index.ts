55a2ffca06d12c8dff5f9cf1d98fb4bef65bc0c2
kirkol94 <kirkol94@ya.ru>
2024-03-14 12:58:38 +0500

    фукнция создания нового пользователя в бд

--- server/src/db/index.ts
+++ server/src/db/index.ts
@@ -1,10 +1,28 @@
 import { Sequelize } from 'sequelize'
 
 import { UserModel } from './models/UserModel'
+import type { IUser } from '../types/IUser'
 
 const sequelize = new Sequelize('sqlite:database.db')
 
-const user = UserModel(sequelize)
+const User = UserModel(sequelize)
 
-user.sync()
-sequelize.sync({ force: true })
+User.sync({ force: true })
+
+const createUser = async (user: Pick<IUser, 'email' | 'password' | 'firstName' | 'lastName' | 'middleName'>) => {
+  try {
+    const newUser = await User.create(user)
+    console.log('Новый пользователь успешно добавлен:', newUser.toJSON())
+  } catch (error) {
+    console.error('Ошибка при создании пользователя:', error)
+  }
+}
+
+const newUser: Pick<IUser, 'email' | 'password' | 'firstName' | 'lastName' | 'middleName'> = {
+  email: 'kirkol94@ya.ru',
+  password: 'qwerty123456',
+  firstName: 'kirill',
+  lastName: 'kolchanov',
+  middleName: 'olegovich',
+}
+createUser(newUser)
