f7e7f21e82a12e07398ee9c7aebe57fdc8e033bb
kirkol94 <kirkol94@ya.ru>
2024-03-15 13:34:04 +0500

    заменил числовые статус коды на более читаемые

--- server/src/middlewares/checkHeaderAuthorization.ts
+++ server/src/middlewares/checkHeaderAuthorization.ts
@@ -1,9 +1,11 @@
 import type { NextFunction, Request, Response } from 'express'
 
+import { HttpStatusCodes } from '../const/HttpStatusCodes'
+
 export const checkHeaderAuthorization = (req: Request, res: Response, next: NextFunction) => {
   if (req.headers.authorization) {
     next()
   } else {
-    res.status(400).json({ message: 'требуется авторизация' })
+    res.status(HttpStatusCodes.UNAUTHORIZED).json({ message: 'требуется авторизация' })
   }
 }
--- server/src/middlewares/validateInput.ts
+++ server/src/middlewares/validateInput.ts
@@ -1,10 +1,12 @@
 import type { NextFunction, Request, Response } from 'express'
 import { validationResult } from 'express-validator'
 
+import { HttpStatusCodes } from '../const/HttpStatusCodes'
+
 export const validateInput = (req: Request, res: Response, next: NextFunction) => {
   const errors = validationResult(req)
   if (!errors.isEmpty()) {
-    res.status(400).json(errors)
+    res.status(HttpStatusCodes.UNPROCESSABLE_ENTITY).json(errors)
   } else {
     next()
   }
