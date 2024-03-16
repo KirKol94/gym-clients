b6f993774b4d34be11a2e2bcf5a9a53aaacfbf6d
kirkol94 <kirkol94@ya.ru>
2024-03-16 19:21:11 +0500

    обновление данных профиля

--- server/src/utils/JWT.ts
+++ server/src/utils/JWT.ts
@@ -8,7 +8,7 @@ export const JWT = {
     return sign(payload, SECRET_KEY, { expiresIn: '24h' })
   },
 
-  decode: (req: Request) => {
+  decode: (req: Request): string | { id: number; email: string } => {
     /**
      * приходит токен вида
      * Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MiwiZW1haWwiOiJhZG1pbjJAZXhhbXBsZS5jb20iLCJpYXQiOjE3MTA0OTM3MDYsImV4cCI6MTcxMDU4MDEwNn0.ANIUIioaeWvL5RZ2wE3Pqet4vGdWphduY8NK_PtOBsc
