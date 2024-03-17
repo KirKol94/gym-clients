a1e32d060c3d0f589aca95109b226ab6ea705a6e
kirkol94 <kirkol94@ya.ru>
2024-03-17 16:53:34 +0500

    server: сохранение изображения

--- server/src/routes/profileRouter.ts
+++ server/src/routes/profileRouter.ts
@@ -1,3 +1,6 @@
+import { writeFile } from 'fs/promises'
+import { resolve } from 'path'
+
 import { Router } from 'express'
 
 import { UserController } from '../controllers/UserController'
@@ -9,6 +12,27 @@ export const profilesRouter = (): Router => {
   router.use(checkHeaderAuthorization)
 
   router.get('/', UserController.getProfile)
+
+  router.put('/img', async (req, res) => {
+    try {
+      const imageData = req.body.avatarFileData
+      const decodedImage = await Buffer.from(imageData, 'base64')
+      const imageName = 'image_' + req.body.firstName + '_' + req.body.lastName + '.jpeg'
+      const imagePath = resolve(__dirname, '..', '..', 'images', imageName)
+
+      await writeFile(imagePath, decodedImage, (err) => {
+        if (err) {
+          console.error(err)
+          res.status(500).send('Ошибка при сохранении изображения')
+        }
+        console.log('Изображение сохранено: ' + imageName)
+        res.send('Изображение успешно сохранено')
+      })
+    } catch (error) {
+      console.log(error)
+      res.send(error)
+    }
+  })
 
   router.put('/update', UserController.updateProfile)
 
