93e9995fb1534395d0c3e653ba29daf362040071
kirkol94 <kirkol94@ya.ru>
2024-03-17 17:06:16 +0500

    проверка на наличие папки images

--- server/src/routes/profileRouter.ts
+++ server/src/routes/profileRouter.ts
@@ -1,7 +1,8 @@
-import { writeFile } from 'fs/promises'
+import { existsSync, mkdir, writeFile } from 'fs'
 import { resolve } from 'path'
+import { promisify } from 'util'
 
-import { Router } from 'express'
+import { type Request, type Response, Router } from 'express'
 
 import { UserController } from '../controllers/UserController'
 import { checkHeaderAuthorization } from '../middlewares/checkHeaderAuthorization'
@@ -13,24 +14,26 @@ export const profilesRouter = (): Router => {
 
   router.get('/', UserController.getProfile)
 
-  router.put('/img', async (req, res) => {
+  router.put('/img', async (req: Request, res: Response) => {
+    const writeFileAsync = promisify(writeFile)
+    const mkdirAsync = promisify(mkdir)
+
     try {
       const imageData = req.body.avatarFileData
-      const decodedImage = await Buffer.from(imageData, 'base64')
-      const imageName = 'image_' + req.body.firstName + '_' + req.body.lastName + '.jpeg'
+      const decodedImage = Buffer.from(imageData, 'base64')
+      const imageName = `image_${req.body.firstName}_${req.body.lastName}.jpeg`
       const imagePath = resolve(__dirname, '..', '..', 'images', imageName)
 
-      await writeFile(imagePath, decodedImage, (err) => {
-        if (err) {
-          console.error(err)
-          res.status(500).send('Ошибка при сохранении изображения')
-        }
-        console.log('Изображение сохранено: ' + imageName)
-        res.send('Изображение успешно сохранено')
-      })
+      const imagesDir = resolve(__dirname, '..', '..', 'images')
+      if (!existsSync(imagesDir)) {
+        await mkdirAsync(imagesDir)
+      }
+
+      await writeFileAsync(imagePath, decodedImage)
+      console.log('Изображение сохранено: ' + imageName)
+      res.send('Изображение успешно сохранено')
     } catch (error) {
-      console.log(error)
-      res.send(error)
+      console.error(error)
     }
   })
 
