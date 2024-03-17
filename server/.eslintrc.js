6cac53d546806a0261f4d579eb26ff041f95e26d
kirkol94 <kirkol94@ya.ru>
2024-03-17 14:54:50 +0500

    правило автоудаления неиспользуемых импортов

--- server/.eslintrc.js
+++ server/.eslintrc.js
@@ -7,7 +7,7 @@ module.exports = {
   env: {
     node: true,
   },
-  plugins: ['@typescript-eslint', 'prettier', 'import', 'prefer-arrow', 'eslint-plugin-prettier'],
+  plugins: ['@typescript-eslint', 'prettier', 'import', 'prefer-arrow', 'eslint-plugin-prettier', 'unused-imports'],
   extends: [
     'eslint:recommended',
     'eslint-config-prettier',
@@ -71,6 +71,17 @@ module.exports = {
       'error',
       {
         tabWidth: 2,
+      },
+    ],
+    'no-unused-vars': 'off', // or "@typescript-eslint/no-unused-vars": "off",
+    'unused-imports/no-unused-imports': 'error',
+    'unused-imports/no-unused-vars': [
+      'warn',
+      {
+        vars: 'all',
+        varsIgnorePattern: '^_',
+        args: 'after-used',
+        argsIgnorePattern: '^_',
       },
     ],
   },
