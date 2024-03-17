37a12d817a9a822a019b843b6311cc3498c96c37
kirkol94 <kirkol94@ya.ru>
2024-03-17 16:53:34 +0500

    server: сохранение изображения

--- server/src/middlewares/logRequestBody.ts
+++ server/src/middlewares/logRequestBody.ts
@@ -3,8 +3,9 @@ import type { NextFunction, Request, Response } from 'express'
 
 export const logRequestBody = (req: Request, res: Response, next: NextFunction) => {
   /** выводим в консоль тело каждого post запроса */
-  if (req.method === 'POST') {
+  if (req.method === 'POST' || req.method === 'PUT') {
     console.log(colors.bgBlue.white(JSON.stringify(req.body, null, '  ')))
+    console.log(colors.bgBlue.yellow(req.url))
   }
 
   next()
