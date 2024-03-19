8c36af927a92915d8bbfe581e3a3ec2acfedf12f
kirkol94 <kirkol94@ya.ru>
2024-03-19 17:17:11 +0500

    front: обернул приложение в error boundary

--- client/src/app/ui/App.tsx
+++ client/src/app/ui/App.tsx
@@ -1,7 +1,9 @@
 import { useEffect } from 'react'
+import { withErrorBoundary } from 'react-error-boundary'
 
 import { Router } from '@/app/routes'
 import { userActions } from '@/entities/User'
+import { ErrorPage } from '@/pages/ErrorPage'
 import { useAppDispatch } from '@/shared/hooks'
 
 import '../styles/index.scss'
@@ -15,3 +17,7 @@ export const App = () => {
 
   return <Router />
 }
+
+export const AppWithErrorBoundary = withErrorBoundary(App, {
+  fallback: <ErrorPage />,
+})
